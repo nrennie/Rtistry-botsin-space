71fcb7f567c422940798361b57a2665374e1ab7c
nrennie <nrennie35@gmail.com>
2023-09-17 17:56:59 +0100

    add mirrored to selection

--- toot_art.R
+++ toot_art.R
@@ -4,21 +4,39 @@ library(rtoot)
 library(ggplot2)
 library(aRt)
 library(usefunc)
+library(PrettyCols)
 
 # Create aRt --------------------------------------------------------------
 
-col1 <- usefunc::random_hex(n = 1)
-col2 <- usefunc::complementary_colour(col1)
 ss <- as.numeric(Sys.time())
-x <- sample(10:30, size = 1)
-d <- stats::runif(1, 0, 1)
-g <- aRt::shatter(n_x = x, n_y = x, decay = d, colour = col1, bg_col = col2, s = ss)
-temp_file <- tempfile(fileext = ".png")
-ggsave(filename = temp_file,
-       plot = g,
-       width = 600,
-       height = 600,
-       units = "px")
+art_type <- sample(c("shatter", "mirrored"), size = 1)
+
+if (art_type == "shatter") {
+  col1 <- usefunc::random_hex(n = 1)
+  col2 <- usefunc::complementary_colour(col1)
+  x <- sample(10:30, size = 1)
+  d <- stats::runif(1, 0, 1)
+  g <- aRt::shatter(n_x = x, n_y = x, decay = d, colour = col1, bg_col = col2, s = ss)
+  temp_file <- tempfile(fileext = ".png")
+  ggsave(filename = temp_file,
+         plot = g,
+         width = 600,
+         height = 600,
+         units = "px")
+} else {
+  if (art_type == "mirrored") {
+    n <- sample(5:20, size = 1)
+    w <- sample(2:6, size = 1)
+    col_palette <- PrettyCols::prettycols(sample(names(PrettyCols::PrettyColsPalettes), size = 1))
+    g <- aRt::mirrored(n = n, w = w, col_palette = col_palette, s = ss)
+    temp_file <- tempfile(fileext = ".png")
+    ggsave(filename = temp_file,
+           plot = g,
+           width = 600,
+           height = 600,
+           units = "px")
+  }
+}
 
 # Text for post -----------------------------------------------------------
 
